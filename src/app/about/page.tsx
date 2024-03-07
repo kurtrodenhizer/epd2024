d54324f136180a219a1d5395c033691f61426e29
Carli <carli.t.smith1@gmail.com>
2024-03-07 11:31:08 -0500

    repush

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <>
       <Header></Header>
-      <AboutPage></AboutPage>
+      <AboutPage />
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ import "./globals.css";
 import Hero from "./ui/hero/Hero";
 import Services from "./ui/services/Services";
 import Clients from "./ui/clients/Clients";
-
-import About from "./ui/about/About";
 import ContacCta from "./ui/contact/contacCta";
+import About from "./ui/about/About";
 
 export default function Home() {
   return (
